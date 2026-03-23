c74ec983dafd8be8a8dcbb0cbaf0d1b9c12a2693
Daniil Shapovalov <shapovalov.pr@gmail.com>
2026-03-23 15:56:00 +0100

    fix: base url for new gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/rss-school/',
+  base: '/pokedex_React-TS/',
   plugins: [react()],
 })
